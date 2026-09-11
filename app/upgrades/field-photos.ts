91a1638f787bf01ae57ffa9b36efdedc60d0f6dc
CRMwebsolution <cody@southernautomate.com>
2026-09-11 14:01:43 -0400

    Add field-photo combiner for the before/after slider

---
+++ app/upgrades/field-photos.ts
@@ -0,0 +1,9 @@
+import { BEFORE_1 } from "./field-before-1";
+import { BEFORE_2 } from "./field-before-2";
+import { BEFORE_3 } from "./field-before-3";
+import { AFTER_1 } from "./field-after-1";
+import { AFTER_2 } from "./field-after-2";
+import { AFTER_3 } from "./field-after-3";
+
+export const BEFORE_IMAGE_URL = `data:image/jpeg;base64,${BEFORE_1}${BEFORE_2}${BEFORE_3}`;
+export const AFTER_IMAGE_URL = `data:image/jpeg;base64,${AFTER_1}${AFTER_2}${AFTER_3}`;
